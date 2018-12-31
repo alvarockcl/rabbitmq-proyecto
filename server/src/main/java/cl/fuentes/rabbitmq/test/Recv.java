8250b08a82049efff7685a65167413d6fb6aec63
Alvaro Fuentes <alvarofue@gmail.com>
2018-12-31 11:25:24 -0300

    Agrega mejoras

--- server/src/main/java/cl/fuentes/rabbitmq/test/Recv.java
+++ server/src/main/java/cl/fuentes/rabbitmq/test/Recv.java
@@ -7,20 +7,24 @@ import com.rabbitmq.client.DeliverCallback;
 
 public class Recv {
 
-    private final static String QUEUE_NAME = "Hola";
+    private final static String QUEUE_NAME = "MICOLA";
 
     public static void main(String[] argv) throws Exception {
+        
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        
         System.out.println(" [*] Esperando por el mensaje. Para salir CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Recivido '" + message + "'");
+        
         };
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
--- server/src/main/java/cl/fuentes/rabbitmq/test/Send.java
+++ server/src/main/java/cl/fuentes/rabbitmq/test/Send.java
@@ -6,18 +6,25 @@ import com.rabbitmq.client.Channel;
 
 public class Send{
 
-    private final static String QUEUE_NAME = "Hola";
+    private final static String QUEUE_NAME = "MICOLA";
     
     public static void main(String[] argv) throws Exception {
     
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+            
+            Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hola Mundo!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Enviado '" + message + "'");
+            
+            //String message = "Mensaje de cola";
+            String message = "";
+
+            for(int i = 1 ; i <=100 ; i++){
+                message = "Mensaje N : " + i;
+               channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+               System.out.println(" [x] Enviado '" + message + "'");
+            }
         }
     }
 }
