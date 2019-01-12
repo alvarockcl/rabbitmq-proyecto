c71b15a6acc3fbfe2b988be7bf866969e0431c84
Alvaro Fuentes <alvarofue@gmail.com>
2019-01-12 13:50:37 -0300

    Agrega RPC

---
+++ netcore/rpc-client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+
+
+namespace rpc_client
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            var rpcClient = new RpcClient();
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+        }
+    }
+}
